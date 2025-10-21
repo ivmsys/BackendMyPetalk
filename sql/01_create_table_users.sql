43a2aec3321ebece30ded590290f81efd641c508
ivmsys <istvanvelamarquez@gmail.com>
2025-10-21 09:10:58 -0600

    Create 01_create_table_users.sql

---
+++ sql/01_create_table_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
